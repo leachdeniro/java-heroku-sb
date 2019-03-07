a84f5f07690801dfd783ccbd2221fd3665b98801
SpeedGabor <leachdeniro@gmail.com>
2019-03-07 14:07:51 -0300

    conversión nombres persona

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,9 +1,8 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
@@ -14,6 +13,11 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                String.format(template, name));
+    }
+    @RequestMapping(value = "/convertir", method = RequestMethod.POST)
+    public Persona convertir(@RequestBody Persona persona) {
+        System.out.println("nombre: " + persona.getNombre() + " ; apellido: " + persona.getApellido());
+        return new Persona(persona.getNombre().toUpperCase(),persona.getApellido().toUpperCase());
     }
 }
